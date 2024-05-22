f2545e28c62701554ac77f99066a2014bb757d90
Ben <b.degens@ilionx.com>
2024-05-22 21:59:44 +0200

    Testfix

--- scenario-weather.js
+++ scenario-weather.js
@@ -1,10 +1,6 @@
-const { chromium } = require('playwright');  // or 'firefox' or 'webkit'
+const visit = async (page) => {
+    await page.goto('weather', { waitUntil: 'networkidle' });
+    await page.scrollToEnd();
+};
 
-(async () => {
-    const browser = await chromium.launch({ headless: true });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('http://localhost:5000/weather');
-    // Add the rest of your scenario steps here
-    await browser.close();
-})();
+module.exports = visit;
